6d308c0d8560ca8a95a52ee30c0f4c93891185b7
EstafaniaDiaz14 <160290649+EstafaniaDiaz14@users.noreply.github.com>
2024-02-20 02:03:19 +0000

    version 1

---
+++ include/Alimento.hpp
@@ -0,0 +1,13 @@
+class Alimento
+{
+private:
+    int Energia;
+public:
+     Alimento(int Energia) {
+        this->Energia = Energia;
+     }
+    ~ Alimento() {}
+    int ExtraerEnergia(){
+        return Energia;
+    }
+};
